13512ea48eb44f4e1bbe4fd06d2c19e3b41ebf24
Matvey Rudoy <mrudybtw@gmail.com>
2024-08-14 09:14:53 +0300

    Base of project was made not enough good

--- cvestream/app/db_updater.py
+++
@@ -1,8 +0,0 @@
-from app.workers.nvd_worker import fetch_nvd_data
-
-def main():
-    # Запуск задачи Celery
-    fetch_nvd_data.apply_async(args=[2020])  # Можно изменить год на нужный
-
-if __name__ == "__main__":
-    main()
